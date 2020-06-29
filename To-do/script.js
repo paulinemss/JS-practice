821e9b08e7467ae572a9496d1e1d506abe8908f9
Pauline Masse <masse.pauline@gmail.com>
2020-06-29 17:28:35 +0200

    Add JS logic for todo list

--- To-do/script.js
+++ To-do/script.js
@@ -1,12 +1,64 @@
 const buttonAddTask = document.getElementById('buttonAddTask'); 
 const addTaskForm = document.getElementById('addTaskForm'); 
+const addTaskInput = document.getElementById('addTaskInput'); 
+const buttonAddInput = document.getElementById('buttonAddInput'); 
+
+const userInput = []; 
+const checkedToDos = []; 
 
 function addTask() {
-    if (addTaskForm.style.display === "none") {
-        addTaskForm.style.display = "block";
+    if (addTaskForm.style.display === 'none') {
+        addTaskForm.style.display = 'block';
     } else {
-        addTaskForm.style.display = "none";
+        addTaskForm.style.display = 'none';
     }
 }
 
+function addInput() {
+    if (addTaskInput.value != '') {
+        userInput.push(addTaskInput.value); 
+        renderToDo(); 
+    }
+    addTaskInput.value = ''; 
+}
+
+function checkUncheck(val) {
+    let elementIndex = checkedToDos.indexOf(val); 
+    if (elementIndex === -1) {
+        checkedToDos.push(val); 
+    } else {
+        checkedToDos.splice(elementIndex, 1); 
+    }
+    renderToDo(); 
+}
+
+function renderToDo() {
+    let str = '';
+    for (let i=0; i<userInput.length; i++) {
+        let todo = userInput[i];
+
+        if (checkedToDos.indexOf(todo) === -1) {
+            str += `<section class="todo">
+            <div class ="todo_checkbox">
+                <button onclick="checkUncheck('${todo}')"><i class="far fa-square"></i></button>
+            </div>
+            <div class="todo_description unchecked">
+                ${todo}
+            </div>
+            </section>`; 
+        } else {
+            str += `<section class="todo">
+            <div class ="todo_checkbox">
+                <button onclick="checkUncheck('${todo}')"><i class="far fa-check-square"></i></button>
+            </div>
+            <div class="todo_description checked">
+                ${todo}
+            </div>
+            </section>`; 
+        }
+    }
+    document.getElementById('allToDos').innerHTML = str;
+}
+
 buttonAddTask.addEventListener('click', addTask); 
+buttonAddInput.addEventListener('click', addInput); 
